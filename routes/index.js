7b8ff0c342007204662e863e46e4d0a607f0564d
Peter Ostiguy <peterjostiguy@gmail.com>
2018-01-25 14:38:59 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@ var db = require('../db/knex.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  return db.select('title', 'name').from('movie').innerJoin('movie_person', 'movie.id', 'movie_person.movie_id').innerJoin('person', 'person.id', 'movie_person.movie_id')
-  .then(function(movieData){
-    console.log(movieData)
-  })
-  res.render('index', { title: 'Express' })
+  console.log("GET GOT!")
 })
 
 module.exports = router;
